20996c532a61c5fac1291c4a7fe6f691940cb54f
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-05 19:51:00 +0900

    🔨 practice

--- ABC/abc412/b/go/main.go
+++ ABC/abc412/b/go/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"unicode"
 )
 
+func ReadLine() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var S, T string
+	fmt.Scan(&S, &T)
+
+	tSet := make(map[rune]bool)
+	for _, char := range T {
+		tSet[char] = true
+	}
+
+	sRunes := []rune(S)
+	for i := 1; i < len(sRunes); i++ {
+		if unicode.IsUpper(sRunes[i]) {
+			prevChar := sRunes[i-1]
+			if !tSet[prevChar] {
+				fmt.Println("No")
+				return
+			}
+		}
+	}
+
+	fmt.Println("Yes")
 }
