62c3af806a0ad464b972ce1874f7428ac1beaee7
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-05 18:07:37 +0900

    update main.go

--- templates/default/go/main.go
+++ templates/default/go/main.go
@@ -1,8 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
+
+func ReadLine() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func ReadInt() int {
+	line := ReadLine()
+	n, _ := strconv.Atoi(line)
+	return n
+}
+
+func ReadInts() []int {
+	line := ReadLine()
+	strs := strings.Fields(line)
+	ints := make([]int, len(strs))
+	for i, str := range strs {
+		ints[i], _ = strconv.Atoi(str)
+	}
+	return ints
+}
 
 func main() {
 	fmt.Println("Hello, World!")
