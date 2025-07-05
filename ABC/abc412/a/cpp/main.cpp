103ff21a173308fc44f76dc42628e9a88b0b2f9f
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-05 18:31:03 +0900

    🔨 practice

--- ABC/abc412/a/cpp/main.cpp
+++ ABC/abc412/a/cpp/main.cpp
@@ -1,21 +1,46 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cmath>
 #include <climits>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+int readInt() {
+  int n;
+  cin >> n;
+  return n;
+}
+
+vector<int> readInts(int n) {
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  return arr;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    cout << "Hello, World!" << endl;
-    
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n = readInt();
+
+  int count = 0;
+
+  for (int i = 0; i < n; i++) {
+    vector<int> data = readInts(2);
+    if (data[1] > data[0]) {
+      count++;
+    }
+  }
+
+  cout << count << endl;
+
+  return 0;
 }
