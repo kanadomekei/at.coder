02259a32874d00d62afd4fa37d4c4dda8316a6b2
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-05 17:53:57 +0900

    zigのテンプレートを更新

--- templates/default/zig/main.zig
+++ templates/default/zig/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
-const print = std.debug.print;
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     // const allocator = gpa.allocator();
-    std.debug.print("Hello, {s}!\n", .{"World"});
+
+    try std.io.getStdOut().writer().print("Hello, {s}!\n", .{"World"});
 }
