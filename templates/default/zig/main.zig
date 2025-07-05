eca994a54f22815452874a7cafa2225bfc79bc0d
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-05 18:37:28 +0900

    update main.zig

--- templates/default/zig/main.zig
+++ templates/default/zig/main.zig
@@ -2,6 +2,40 @@ const std = @import("std");
 
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+
+pub fn readLine(allocator: Allocator) ![]u8 {
+    const stdin = std.io.getStdIn().reader();
+    return try stdin.readUntilDelimiterAlloc(allocator, '\n', 1024 * 1024);
+}
+
+pub fn readInt(comptime T: type) !T {
+    var buffer: [64]u8 = undefined;
+    const stdin = std.io.getStdIn().reader();
+    if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
+        const trimmed = std.mem.trim(u8, line, " \t\n\r");
+        return std.fmt.parseInt(T, trimmed, 10);
+    }
+    return error.EndOfStream;
+}
+
+pub fn readInts(comptime T: type, allocator: Allocator) ![]T {
+    const line = try readLine(allocator);
+    defer allocator.free(line);
+
+    var result = ArrayList(T).init(allocator);
+
+    var iter = std.mem.splitSequence(u8, line, " ");
+    while (iter.next()) |token| {
+        const trimmed = std.mem.trim(u8, token, " \t\n\r");
+        if (trimmed.len > 0) {
+            const num = try std.fmt.parseInt(T, trimmed, 10);
+            try result.append(num);
+        }
+    }
+
+    return result.toOwnedSlice();
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
