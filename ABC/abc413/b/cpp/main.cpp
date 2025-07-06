440bcf90ee7bb52a1534926cfd9759c7d701cd63
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-06 16:04:34 +0900

    🔨 abc413_b(c++)

--- ABC/abc413/b/cpp/main.cpp
+++ ABC/abc413/b/cpp/main.cpp
@@ -1,35 +1,64 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cmath>
 #include <climits>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int readInt() {
-    int n;
-    cin >> n;
-    return n;
+  int n;
+  cin >> n;
+  return n;
 }
 
 vector<int> readInts(int n) {
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    return arr;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  return arr;
+}
+
+string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+
+vector<string> readStrings(int n) {
+  vector<string> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  return arr;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    cout << "Hello, World!" << endl;
-    
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n = readInt();
+
+  vector<string> s = readStrings(n);
+
+  set<string> uniqueStrings;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == j) {
+        continue;
+      }
+      uniqueStrings.insert(s[i] + s[j]);
+    }
+  }
+
+  cout << uniqueStrings.size() << endl;
+
+  return 0;
 }
