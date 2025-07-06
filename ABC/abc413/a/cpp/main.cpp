fd806f9d99e59e7fbe32c4424551067e2be07d02
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-06 14:47:49 +0900

    🔨 abc413

--- ABC/abc413/a/cpp/main.cpp
+++ ABC/abc413/a/cpp/main.cpp
@@ -1,35 +1,47 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cmath>
 #include <climits>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int readInt() {
-    int n;
-    cin >> n;
-    return n;
+  int n;
+  cin >> n;
+  return n;
 }
 
 vector<int> readInts(int n) {
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    return arr;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  return arr;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    cout << "Hello, World!" << endl;
-    
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  n = readInt();
+  m = readInt();
+  vector<int> a = readInts(n);
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+  }
+  if (sum <= m) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
 }
