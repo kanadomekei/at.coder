24f75583316b087fc16390ec96d47e3612daf2aa
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-06 18:36:04 +0900

    🔨 abc413_c(go)

--- ABC/abc413/c/go/main.go
+++ ABC/abc413/c/go/main.go
@@ -8,8 +8,14 @@ import (
 	"strings"
 )
 
+type Pair struct {
+	X int64
+	C int64
+}
+
+var scanner = bufio.NewScanner(os.Stdin)
+
 func ReadLine() string {
-	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
 }
@@ -31,5 +37,32 @@ func ReadInts() []int {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	q := ReadInt()
+
+	list := []Pair{}
+	head := 0
+
+	for i := 0; i < q; i++ {
+		query := ReadInts()
+		if query[0] == 1 {
+			list = append(list, Pair{X: int64(query[2]), C: int64(query[1])})
+		} else {
+			k := int64(query[1])
+			var sum int64 = 0
+
+			for k > 0 && head < len(list) {
+				if list[head].C <= k {
+					sum += list[head].X * list[head].C
+					k -= list[head].C
+					head++
+				} else {
+					sum += list[head].X * k
+					list[head].C -= k
+					k = 0
+				}
+			}
+			fmt.Println(sum)
+		}
+	}
+
 }
