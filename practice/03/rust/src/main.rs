e13e2872dc887c2f81372350ed6ada37809a0079
kanadomekei <tsuchiyakeita2025@gmail.com>
2025-07-08 12:42:44 +0900

    🔨 practice03(rust)

---
+++ practice/03/rust/src/main.rs
@@ -0,0 +1,39 @@
+#![recursion_limit = "256"]
+
+use proconio::input;
+// use itertools::Itertools;
+// use std::cmp::{max, min};
+// use std::collections::{HashMap, HashSet, VecDeque};
+
+fn main() {
+    // For recursive functions, prevent stack overflow.
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| {
+            solve();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn solve() {
+    input! {
+        h: usize,
+        a: usize,
+    }
+
+    // Calculate the answer with ceiling division
+    let ans = (h as f64 / a as f64).ceil() as usize;
+
+    println!("{}", ans);
+}
+
+// Macro for debugging
+#[macro_export]
+macro_rules! dbg {
+    ($($a:expr),* $(,)?) => {
+        #[cfg(debug_assertions)]
+        eprintln!(concat!($("| ", stringify!($a), " = {:?} "),*, "|"), $(&$a),*);
+    };
+}
